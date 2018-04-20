7deb96b22ac64f93ff59ff2c4a3c568c93371cb4
KingVictoria <TheRealKingVictoria@gmail.com>
2018-04-19 23:58:01 -0400

    FactionHandler fully set up -- all that is left is to test

--- src/main/java/com/gmail/therealkingvictoria/CivFactions.java
+++ src/main/java/com/gmail/therealkingvictoria/CivFactions.java
@@ -3,7 +3,7 @@ package com.gmail.therealkingvictoria;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import factions.FactionHandler;
+import com.gmail.therealkingvictoria.factions.FactionHandler;
 
 public class CivFactions extends JavaPlugin {
   Plugin plugin;
--- src/main/java/com/gmail/therealkingvictoria/factions/FactionHandler.java
+++ src/main/java/com/gmail/therealkingvictoria/factions/FactionHandler.java
@@ -1,12 +1,19 @@
 package com.gmail.therealkingvictoria.factions;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.logging.Level;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 public class FactionHandler {
   ArrayList<Faction> factions;
   Plugin plugin;
+  File factionConfigFile;
+  FileConfiguration factionConfig;
 
   /**
    * Creates a FactionHandler object that handles the Factions
@@ -14,13 +21,34 @@ public class FactionHandler {
    */
   public FactionHandler(Plugin plugin) {
     this.plugin = plugin;
+
   } // FactionHandler
 
+  /**
+   * Loads factions from factions.yml config file
+   */
   public void load() {
-    // TODO: load
+    factionConfigFile = new File(plugin.getDataFolder(), "factions.yml");
+    factionConfig = YamlConfiguration.loadConfiguration(factionConfigFile);
+
+    factions = new ArrayList<>();
+
+    ArrayList<Map<String, Object>> serializedFactions = (ArrayList<Map<String, Object>>) factionConfig.get("factions");
+    for(Map<String, Object> map: serializedFactions) factions.add(new Faction(map));
   } // load
 
+  /**
+   * Saves factions to factions.yml config file
+   */
   public void save() {
-    // TODO: save
+    ArrayList<Map<String, Object>> serializedFactions = new ArrayList<>();
+    for(Faction faction: factions) serializedFactions.add(faction.serialize());
+    factionConfig.set("factions", serializedFactions);
+    
+    try {
+      factionConfig.save(factionConfigFile);
+    } catch (Exception e) {
+      plugin.getLogger().log(Level.SEVERE, "Could not save config to " + factionConfigFile, e);
+    }
   } // save
 } // FactionHandler

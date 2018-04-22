e309660538c6f68c262adcca7a29ab521557f9b8
KingVictoria <TheRealKingVictoria@gmail.com>
2018-04-22 19:27:38 -0400

    Added basis for Estates

---
+++ src/main/java/com/gmail/therealkingvictoria/estates/Estate.java
@@ -0,0 +1,39 @@
+package com.gmail.therealkingvictoria.estates;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class Estate {
+
+  Location center;
+  
+  public Estate(Location loc) {
+    center = loc;
+  } // Estate(Location)
+
+  public Estate(Map<String, Object> map) {
+    HashMap<String, Object> location = (HashMap<String, Object>) map.get("location");
+    String world = (String) map.get("world");
+    double x = (int) map.get("x");
+    double y = (int) map.get("y");
+    double z = (int) map.get("z");
+    center = new Location(Bukkit.getWorld(world), x, y, z);
+  } // Estate(Map<String, Object)
+
+  public Map<String, Object> serialize() {
+    HashMap<String, Object> map = new HashMap<>();
+
+    HashMap<String, Object> location = new HashMap<>();
+    location.put("world", center.getWorld().getName());
+    location.put("x", center.getBlockX());
+    location.put("y", center.getBlockY());
+    location.put("z", center.getBlockZ());
+    map.put("location", location);
+
+    return map;
+  } // serialize
+
+} // Estate

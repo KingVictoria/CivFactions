a705a2f63a75bf0f21ca65913b244ed26dcb1223
KingVictoria <TheRealKingVictoria@gmail.com>
2018-04-29 17:53:12 -0400

    Added foundation for Estates

--- src/main/java/com/gmail/therealkingvictoria/estates/Estate.java
+++ src/main/java/com/gmail/therealkingvictoria/estates/Estate.java
@@ -8,32 +8,48 @@ import org.bukkit.Location;
 
 public class Estate {
 
-  Location signLoc;
+  String name;
+  Location location;
+
+  int resBonus; // resource bonus level
   
-  public Estate(Location loc) {
-    signLoc = loc;
+  public Estate(String name, Location loc) {
+    this.name = name;
+    location = loc;
+    resBonus = 1;
   } // Estate(Location)
 
   public Estate(Map<String, Object> map) {
-    HashMap<String, Object> location = (HashMap<String, Object>) map.get("location");
-    String world = (String) map.get("world");
-    double x = (int) map.get("x");
-    double y = (int) map.get("y");
-    double z = (int) map.get("z");
-    signLoc = new Location(Bukkit.getWorld(world), x, y, z);
+    HashMap<String, Object> loc = (HashMap<String, Object>) map.get("location");
+    String world = (String) loc.get("world");
+    double x = (int) loc.get("x");
+    double y = (int) loc.get("y");
+    double z = (int) loc.get("z");
+    location = new Location(Bukkit.getWorld(world), x, y, z);
+
+    name = (String) map.get("name");
+
+    resBonus = (int) map.get("resource_bonus");
   } // Estate(Map<String, Object)
 
   public Map<String, Object> serialize() {
     HashMap<String, Object> map = new HashMap<>();
 
-    HashMap<String, Object> location = new HashMap<>();
-    location.put("world", signLoc.getWorld().getName());
-    location.put("x", signLoc.getBlockX());
-    location.put("y", signLoc.getBlockY());
-    location.put("z", signLoc.getBlockZ());
+    HashMap<String, Object> loc = new HashMap<>();
+    loc.put("world", location.getWorld().getName());
+    loc.put("x", location.getBlockX());
+    loc.put("y", location.getBlockY());
+    loc.put("z", location.getBlockZ());
     map.put("location", location);
+
+    map.put("name", name);
+
+    map.put("resource_bonus", resBonus);
 
     return map;
   } // serialize
 
+  public String   getName()     { return name;     }
+  public Location getLocation() { return location; }
+  public int      getResBonus() { return resBonus; }
 } // Estate
